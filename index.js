6f4f88f63cb8e2628789e1ab96d0924aa61c77da
Priyanshu Mishra <priyanshu12651265@gmail.com>
2023-06-20 16:56:03 +0530

    Added Cross Icon to List items

--- index.js
+++ index.js
@@ -13,32 +13,32 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value
-    
+
     push(shoppingListInDB, inputValue)
-    
+
     clearInputFieldEl()
 })
 
-onValue(shoppingListInDB, function(snapshot) {
+onValue(shoppingListInDB, function (snapshot) {
 
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-    
+
         clearShoppingListEl()
-        
+
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
             let currentItemID = currentItem[0]
             let currentItemValue = currentItem[1]
-            
+
             appendItemToShoppingListEl(currentItem)
-        }    
+        }
     } else {
         shoppingListEl.innerHTML = "Nothing here... yet!"
     }
-    
+
 })
 
 function clearShoppingListEl() {
@@ -49,19 +49,27 @@ function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
+function listItemGenerator(itemValue, itemID) {
+    const listItem = document.createElement("li")
+    listItem.textContent = itemValue
+
+    const crossIcon = document.createElement('button')
+    crossIcon.textContent = 'X'
+    crossIcon.addEventListener("click", function () {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
+        remove(exactLocationOfItemInDB)
+    })
+    listItem.appendChild(crossIcon)
+
+    return listItem
+}
+
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = itemValue
-    
-    newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
-        remove(exactLocationOfItemInDB)
-    })
-    
-    shoppingListEl.append(newEl)
+
+    const newItem = listItemGenerator(itemValue, itemID)
+
+    shoppingListEl.append(newItem)
 }
